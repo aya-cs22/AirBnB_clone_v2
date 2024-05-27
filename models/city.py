9d707a93fc7b379adff192113c68aa753fa707d1
aya-cs22 <aya786930@gmail.com>
2024-05-27 09:33:52 +0300

    DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -6,17 +6,6 @@ from sqlalchemy import Column, String, ForeignKey
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-     __tablename__ = 'states'
+     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """Cities getter"""
-            from models.city import City
-            from models import storage
-            list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    list.append(city)
-            return list
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
--- models/state.py
+++ models/state.py
@@ -5,4 +5,17 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Cities getter"""
+            from models.city import City
+            from models import storage
+            list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    list.append(city)
+            return list
