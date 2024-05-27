2c9391500432378e51bbd7bb6b2ee2d65fbc02b2
aya-cs22 <aya786930@gmail.com>
2024-05-27 09:51:03 +0300

    DBStorage - States and Cities

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -38,50 +38,89 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the current database session all objects depending on the class name."""
-        objects_dict = {}
-        if cls:
-            objects = self.__session.query(cls).all()
-            for obj in objects:
-                key = f'{obj.__class__.__name__}.{obj.id}'
-                objects_dict[key] = obj
+        """query on the current database session"""
+        objs = []
+        dct = {}
+        if cls is None:
+            for item in self.classes:
+                objs.extend(self.__session.query(item).all())
         else:
-            for subclass in Base.__subclasses__():
-                objects = self.__session.query(subclass).all()
-                for obj in objects:
-                    key = f'{obj.__class__.__name__}.{obj.id}'
-                    objects_dict[key] = obj
-        return objects_dict
+            if type(cls) is str:
+                cls = eval(cls)
+            objs = self.__session.query(cls).all()
+
+        for obj in objs:
+            dct[obj.__class__.__name__ + '.' + obj.id] = obj
+        return dct
 
     def new(self, obj):
-        """Add the object to the current database session."""
+        """add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes of the current database session."""
+        """commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete from the current database session obj if not None."""
-        if obj:
+        """delete from the current database session obj if not None"""
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and create the current database session."""
-        from models.base_model import BaseModel, Base
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
+        """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
-
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
     def close(self):
-        """Close the current session."""
+        """call remove() method on the private session attribute"""
         self.__session.close()
+    # def all(self, cls=None):
+    #     """Query on the current database session all objects depending on the class name."""
+    #     objects_dict = {}
+    #     if cls:
+    #         objects = self.__session.query(cls).all()
+    #         for obj in objects:
+    #             key = f'{obj.__class__.__name__}.{obj.id}'
+    #             objects_dict[key] = obj
+    #     else:
+    #         for subclass in Base.__subclasses__():
+    #             objects = self.__session.query(subclass).all()
+    #             for obj in objects:
+    #                 key = f'{obj.__class__.__name__}.{obj.id}'
+    #                 objects_dict[key] = obj
+    #     return objects_dict
+
+    # def new(self, obj):
+    #     """Add the object to the current database session."""
+    #     self.__session.add(obj)
+
+    # def save(self):
+    #     """Commit all changes of the current database session."""
+    #     self.__session.commit()
+
+    # def delete(self, obj=None):
+    #     """Delete from the current database session obj if not None."""
+    #     if obj:
+    #         self.__session.delete(obj)
+
+    # def reload(self):
+    #     """Create all tables in the database and create the current database session."""
+    #     from models.base_model import BaseModel, Base
+    #     from models.user import User
+    #     from models.state import State
+    #     from models.city import City
+    #     from models.amenity import Amenity
+    #     from models.place import Place
+    #     from models.review import Review
+
+    #     Base.metadata.create_all(self.__engine)
+
+    #     session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+    #     Session = scoped_session(session_factory)
+    #     self.__session = Session()
+
+    # def close(self):
+    #     """Close the current session."""
+    #     self.__session.close()
